b9ea848064e4622c446e807d11a29daf40d50736
Pranali Ahirrao <55869366+pranaliahirrao@users.noreply.github.com>
2023-09-10 23:50:13 +0530

    understand function stack concept

--- Array/Intro.cpp
+++ Array/Intro.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace  std ;
 
+int fun2(int a)
+{
+    cout<<a<<endl ;
+    cout<<"Its function 2"<<endl;
+    return a;
+}
+
+int fun1(int a)
+{
+    int  c= fun2(a);
+    cout<<"Its function 1"<<endl;
+    return c ;
+}
 
 int main()
 {
-    cout<<"hello";
+    cout<<"hello"<<endl;
+    int a = fun1(5);
+    cout<<a<<endl;
     return 0 ;
 }
