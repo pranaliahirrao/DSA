1a6bfd324c86b73207a1dd7267907b910111f75d
Pranali Ahirrao <55869366+pranaliahirrao@users.noreply.github.com>
2023-10-01 12:37:31 +0530

    Assgn 3 : Array

--- Array/Assgn.cpp
+++ Array/Assgn.cpp
@@ -12,6 +12,15 @@ using namespace std;
 // missing elements from an array with Duplicates - notebook  [ findMissing , findMissing2]
 // find 1st repeating element - gfg/ nb 
 // common elements [gfg] in 3 sorted arrays - gfg / nb 
+// Wave print a Matrix 
+// SPiral Matrix - LC : 54 
+// GFG -> Add two numbers represented by two arrays
+// GFG -> Factorials of large numbers  [ vvvimp ] 
+// lc : 26 ->  Remove duplicate from Sorted array 
+// Lc 643 - Maximum average subarray I
+
+
+
 
 
 
@@ -64,12 +73,57 @@ void findMissing2(int *arr , int n)
 }
 
 
+// Wave print a Matrix - columnwise 
+// wave form - Row wise : HW 
+void wavePrintMatrix(vector<vector<int>> v)
+{
+    int m = v.size();
+    int n = v[0].size() ;
+
+    for(int startCol=0 ; startCol < n ; startCol++)
+    {
+        if((startCol & 1) == 0)
+        {
+            // even numb COl - top to bottom
+            for(int i=0 ; i<m ; i++){
+                cout<<v[i][startCol]<<"  " ;
+            }
+        }
+        else{
+            // odd numb COl - bottom to top
+            for(int i=m-1 ; i>=0 ; i--){
+                cout<<v[i][startCol]<<"  " ;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
 int main()
 {
-    int arr[] = {1, 3 , 5, 3, 3};
-    int n = 5;
+    // int arr[] = {1, 3 , 5, 3, 3};
+    // int n = 5;
     // findMissing(arr, n);
-    findMissing2(arr, n);
+    // findMissing2(arr, n);
+
+
+    vector<vector<int>> v{
+        {1, 2, 3, 4} ,
+        {5, 6, 7, 8} ,
+        {9, 10, 11, 12}
+    };
+
+    wavePrintMatrix(v);
+
+
+
+
 
     return 0 ;
 }
