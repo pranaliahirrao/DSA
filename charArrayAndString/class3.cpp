6115e8618312404821ca316ef80ea3f7982f1b4e
Pranali Ahirrao <55869366+pranaliahirrao@users.noreply.github.com>
2023-10-03 23:14:06 +0530

    assgn 4

--- charArrayAndString/class3.cpp
+++ charArrayAndString/class3.cpp
@@ -1,42 +1,21 @@
 #include <bits/stdc++.h>
-#include <string.h>
 using namespace std;
-
 
 
 
 // LC 2325 : Decode the Message 
 
 
-
-
-
 // LC 2391 : Minimum amt of time to collect garbage 
 
 
-
-
-
-// LC 791 : Custom Sort String
-
-
-
-
+// LC 791 : Custom Sort String - repeat 
 
 
 // LC 890 : Find & Replace Pattern
 
 
-
-
-
 // LC 2125 : Numb of Laser beams in a Bank  ==>> HW
-
-
-
-
-
-
 
 
 
---
+++ searching/Assgn4.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// lc 530 : K-diff Pairs in an Array
+// Method 1 : Two POinter approach
+   int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin() , nums.end());
+        set<pair<int , int>> ans ;   // set bcz we want Unique elements 
+        int i=0 , j=1 ;
+        while(j < nums.size())
+        {
+            int diff = nums[j] - nums[i];
+            if(diff == k){
+                ans.insert({nums[i] , nums[j]}) ;
+                i++ ; j++ ;
+            }
+            else if( diff > k){
+                i++ ;
+            }
+            else{
+                j++ ;     // diff < k
+            }
+            if( i == j){
+                j++ ;    // when both adj elem are same 
+            }
+        }
+        return ans.size() ;
+    }
+// Method 2 : Binary search 
+
+
+
+
+// LC 658 : Find K-closest elements
+
+
+
+
+
+int main()
+{
+
+    
+
+    return 0;
+}
+
+
+
