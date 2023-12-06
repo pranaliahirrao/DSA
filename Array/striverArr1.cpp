3e0b7920a1663b6ec55463e0841d35110e8cbc8f
Pranali Ahirrao <55869366+pranaliahirrao@users.noreply.github.com>
2023-12-06 19:38:00 +0530

    more Qs on Arrays - striver 2nd lec

--- Array/striverArr1.cpp
+++ Array/striverArr1.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std ;
 
 // find largest element in an array
@@ -86,7 +88,7 @@ int removeDuplicates(int arr[], int n)
 }
 
 // Left rotate the array by one place 
-int leftRotateArr(int arr[], int n)
+void leftRotateArr(int arr[], int n)
 {
     int temp = arr[0];
     for(int i=1 ; i<n ; i++)
@@ -98,9 +100,128 @@ int leftRotateArr(int arr[], int n)
 
 // Left rotate the array by K-place 
 // k = num % k  [ if num=15 & k=7 then k = 15%7 = 1 ]
+// method - 1st 
+void leftRotateByD(int arr[], int n , int d)
+{
+    d = d%n ;
+    int temp[d] ;
+    for(int i=0 ; i<d ; i++)
+    {
+        temp[i] = arr[i] ;
+    }
+
+    for(int i=d ; i<n ; i++)
+    {
+        arr[i-d] = arr[i] ;
+    }
+
+    for(int i=n-d ; i<n ; i++)
+    {
+        arr[i] = temp[i-(n-d)] ;
+    }
+}
+// method 2nd => tc = O(2n) & sc=O(1)
+void rotateElemByK(int arr[], int n , int k)
+{
+    // this is for left rorate array by K 
+    reverse(arr, arr+k);
+    reverse(arr+k, arr+n);
+    reverse(arr, arr+n);
+}
+
+// {1,2,3 ,4 ,5} = 5 4 3 2 1 =  3 4 5 2 1
+// d = 3                        
+// {3,  4, 5, 1, 2 }     
+void rightRorateArrbyK(int arr[] , int n , int k)
+{
+    reverse(arr, arr+n);      // 5 4 3 2 1
+    reverse(arr, arr+k);     //  3 4 5 2 1
+    reverse(arr+k, arr+n) ; //   3 4 5 1 2 
+}
+
+// move zero's to end 
+void moveZeros(int arr[], int n)
+{
+    int j=-1 ;
+    for(int i=0 ; i<n ; i++)
+    {
+        if(arr[i] == 0)
+        {
+            j = i ;
+            break ;
+        }
+    }
+
+    if(j == -1) return ;
+
+    for(int i=j+1 ; i<n ; i++)
+    {
+        if(arr[i] != 0 )
+        {
+            swap(arr[i], arr[j]) ;
+            j++ ;
+        }
+    }
+}
+
+// linear search of element in array - simple 
+
+// Finde the Union of two sorted arrays 
+// Note : No repetation odf element is allowed in Union [ should be unique ]
+vector <int> FindUnion(int arr1[], int arr2[], int n, int m)
+{
+     int i = 0, j = 0;  
+  vector < int > Union; 
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+
+}
 
 
-
+// Find Intersection of 2 array - repeat element is allowed here 
+vector <int> findIntersection(int arr1[], int arr2[], int n, int m)
+{
+    vector<int> ans ;
+    int i=0, j=0 ; 
+    while(i<n && j<m)
+    {
+        if(arr1[i] < arr2[j]){
+            i++ ;
+        }
+        else if(arr1[i] > arr2[j]){
+            j++ ;
+        }
+        else{
+            ans.push_back(arr1[i]);
+            i++, j++ ;
+        }
+    }
+    return ans ;
+}
 
 
 
@@ -135,11 +256,28 @@ int main()
     int size3 = 9 ;
     // cout<<removeDuplicates(arr3, size3);
 
-    int arr4[] = {1, 2, 3, 4, 5, 6};
-    int size4 = 6 ;
-    leftRotateArr(arr4, size4);
-    printArray(arr4, size4);
+    int arr4[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int size4 = 8 ;
+    // leftRotateArr(arr4, size4);
+    // printArray(arr4, size4);
 
+    int d = 5 ;
+    // leftRotateByD(arr4, size4, d);
+    // rotateElemByK(arr4, size4, d);
+    // rightRorateArrbyK(arr4, size4, d);
+    // printArray(arr4, size4);
+
+    int arr5[] = {1, 2, 3, 5, 6, 6, 7};
+    int size5 = 7 ;
+    // moveZeros(arr5, size5);
+    // printArray(arr5, size5);
+
+    int arr6[] = {1,3,6, 6 ,9};
+    int size6 = 5 ;
+    // vector <int> Union = FindUnion(arr5, arr6, size5, size6) ;
+    vector<int> intersection = findIntersection(arr5, arr6, size5, size6) ;
+    for (auto & val: intersection)
+       cout << val << " ";
 
 
     return 0 ;
